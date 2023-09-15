d8e2b9645fab7c8852ceca9863dc779a6470d5e4
juha44242 <104754622+juha44242@users.noreply.github.com>
2023-09-15 09:16:16 +0200

    det er ur med nedtælling

---
+++ pentsum/basket_clock.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const addToBasketButtons = document.querySelectorAll(".add-to-basket");
+    const basketItems = document.querySelector(".basket-items");
+    const timer = document.getElementById("timer");
+
+    let basket = [];
+    let minutes = 10;
+    let seconds = 0;
+
+    // Update the timer display
+    function updateTimer() {
+        timer.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    }
+
+    // Decrement the timer
+    function decrementTimer() {
+        if (seconds === 0) {
+            if (minutes === 0) {
+                // Time's up
+                clearInterval(timerInterval);
+                timer.textContent = "Time's up!";
+                return;
+            }
+            minutes--;
+            seconds = 59;
+        } else {
+            seconds--;
+        }
+        updateTimer();
+    }
+
+    const timerInterval = setInterval(decrementTimer, 1000);
+
+    // Add item to the basket
+    function addItemToBasket(itemName, itemPrice) {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${itemName} - ${itemPrice}`;
+        basketItems.appendChild(listItem);
+    }
+
+    addToBasketButtons.forEach((button, index) => {
+        button.addEventListener("click", () => {
+            const item = document.querySelectorAll(".item-name")[index];
+            const price = document.querySelectorAll(".item-price")[index].textContent;
+            basket.push({ name: item.textContent, price });
+            addItemToBasket(item.textContent, price);
+        });
+    });
+});
